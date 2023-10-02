9382c153f90a773852d08919da41b2bcda0e0216
Five Grant <5@fivegrant.com>
2023-10-02 16:31:24 -0500

    Fix report bugs

--- tests/report.py
+++ tests/report.py
@@ -1,13 +1,14 @@
 import os
 import json
 from time import sleep, time
+from datetime import datetime
 
 import boto3
 import requests
 
 TDS_URL = os.environ.get("TDS_URL", "http://data-service:8000")
-PYCIEMSS_URL = os.environ.get("PYCIEMSS_URL", "http://pyciemss-api")
-SCIML_URL = os.environ.get("SCIML_URL", "http://sciml-service")
+PYCIEMSS_URL = os.environ.get("PYCIEMSS_URL", "http://pyciemss-api:8000")
+SCIML_URL = os.environ.get("SCIML_URL", "http://sciml-service:8080")
 BUCKET = os.environ.get("BUCKET", None)
 
 
@@ -15,10 +16,14 @@ def eval_integration(service_name, endpoint, request):
     start_time = time()
     is_success = False
     base_url = PYCIEMSS_URL if service_name == "pyciemss" else SCIML_URL
-    kickoff_request = requests.post(f"{base_url}/{endpoint}", data=request)
+    kickoff_request = requests.post(f"{base_url}/{endpoint}", json=request, 
+        headers= {
+            "Content-Type": "application/json"
+        }    
+    )
     if kickoff_request.status_code < 300:
         sim_id = kickoff_request.json()["simulation_id"]
-        get_status = lambda: request.get(f"{base_url}/{endpoint}/status/{sim_id}").json()["status"]
+        get_status = lambda: requests.get(f"{base_url}/{endpoint}/status/{sim_id}").json()["status"]
         while get_status() in ["queued", "running"]:
             sleep(1)
         if get_status() == "complete":
@@ -32,8 +37,8 @@ def eval_integration(service_name, endpoint, request):
 def gen_report():
     report = {
         "scenarios": {
-            "PyCIEMSS": {},
-            "SciML": {}
+            "pyciemss": {},
+            "sciml": {}
         }, 
         "services": {
             "TDS": {
@@ -51,22 +56,25 @@ def gen_report():
     
     report["scenarios"] = {name: {} for name in os.listdir("scenarios")}
     for scenario in report["scenarios"]:
-        scenario_spec = {
-            "pyciemss": os.listdir(f"scenarios/{scenario}/pyciemss"),
-            "sciml": os.listdir(f"scenarios/{scenario}/sciml")
-        }
+        scenario_spec = {}
+        for backend in ["pyciemss", "sciml"]:
+            path = f"scenarios/{scenario}/{backend}"
+            if os.path.exists(path):
+                scenario_spec[backend] = os.listdir(f"scenarios/{scenario}/{backend}")
         for service_name, tests in scenario_spec.items():
             for test_file in tests:
-                test = test_file.strip(".json")
+                test = test_file.split(".")[0]
                 name = f"{service_name}-{test}"
-                report["scenarios"][scenario][name] = eval_integration(service_name, test, json.load(f"scenarios/{scenario}/{test_file}"))
+                file = open(f"scenarios/{scenario}/{service_name}/{test_file}", "rb") 
+                report["scenarios"][scenario][name] = eval_integration(service_name, test, json.load(file))
     return report
 
 
 def publish_report(report, upload):
-    timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
     filename = f"report_{timestamp}.json"
     fullpath = os.path.join("reports", filename)
+    os.makedirs("reports", exist_ok=True)
     with open(fullpath, "w") as file:
         json.dump(report, file, indent=2)
 
@@ -77,7 +85,7 @@ def publish_report(report, upload):
 
 
 def report(upload=True):
-    publish_report(gen_report())
+    publish_report(gen_report(), upload)
 
 
 if __name__ == "__main__":
--- tests/seed.py
+++ tests/seed.py
@@ -14,17 +14,17 @@ for config_path in model_configs:
         "Content-Type": "application/json"
     })
     if model_response.status_code >= 300:
-        raise Exception(f"Failed to PUT model ({model_response.status_code}): {config['id']}")
+        raise Exception(f"Failed to POST model ({model_response.status_code}): {config['id']}")
     config["model_id"] = model_response.json()["id"]
     config_response = requests.post(TDS_URL + "/model_configurations", json=config,
-        headers={
+        headers= {
             "Content-Type": "application/json"
         }    
     )
 
     
     if config_response.status_code >= 300:
-        raise Exception(f"Failed to PUT config ({config_response.status_code}): {config['id']}")
+        raise Exception(f"Failed to POST config ({config_response.status_code}): {config['id']}")
 
 datasets = glob("./data/datasets/*.csv")
 for filepath in datasets:
@@ -38,12 +38,12 @@ for filepath in datasets:
         ]
     }
     dataset_response = requests.post(TDS_URL + "/datasets", json=dataset,
-        headers={
+        headers= {
             "Content-Type": "application/json"
         }    
     )
     if dataset_response.status_code >= 300:
-        raise Exception(f"Failed to PUT dataset ({dataset_response.status_code}): {dataset['name']}")
+        raise Exception(f"Failed to POSt dataset ({dataset_response.status_code}): {dataset['name']}")
     url_response = requests.get(TDS_URL + f"/datasets/{dataset_name}/upload-url", params={"filename": filename})
     upload_url = url_response.json()["url"]
     with open(filepath, "rb") as file:
